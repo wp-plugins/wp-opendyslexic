beeed5e69a587062945bb3faee8992c0c377a1ea
120w <120w@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-24 12:41:07 +0000

    Adding first version of WP-OpenDyslexic to SVN repository
    git-svn-id: https://plugins.svn.wordpress.org/wp-opendyslexic/trunk@1074819 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Plugin Name: WP-OpenDyslexic
+Plugin URI: https://frankleonard.nl/
+Description: Allows registered users to enable the OpenDyslexic font in their profile.
+Version: 1.0
+Author: Frank Gerritse
+Author URI: https://frankleonard.nl/
+License: GPL2
+Text Domain: opendyslexic
+Domain Path: /languages/
+
+*/
+/*  Copyright 2015  Frank Gerritse (email : frank@frankleonard.nl)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+Changelog:
+20150123 - first version
+*/
+?>
+<?php
+function load_opendyslexic_textdomain() {
+  load_plugin_textdomain( 'opendyslexic', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+ 
+add_action('plugins_loaded', 'load_opendyslexic_textdomain' );
+add_action('profile_personal_options', 'add_opendyslexic_option_to_profile');
+add_action('edit_user_profile_update', 'update_profile_use_opendyslexic'); //deze lijkt niet te werken
+add_action('personal_options_update', 'update_profile_use_opendyslexic');
+add_action('wp_head','add_opendyslexic_css');
+add_action('admin_head', 'add_opendyslexic_css_admin');
+
+function add_opendyslexic_option_to_profile( $user ) {
+    ?>
+        <table class="form-table">
+                <tr>
+                        <th><label><?php _e('Opendyslexic font', 'opendyslexic');?></label></th>
+                        <td><p><?php _e('You can use the OpenDyslexic font on the website or on both the website and the admin. The OpenDyslexic font is designed to help people with dyslexia with their reading. ', 'opendyslexic');?></p></td>
+                </tr>
+        <tr>
+        <td></td>
+        <td>
+ <select name="use_opendyslexic" id="use_opendyslexic" >
+                        <option value="no" <?php selected( 'no', get_user_meta( $user->ID, 'use_opendyslexic', true ) ); ?>><?php _e('Don\'t use the OpenDyslexic font', 'opendyslexic');?></option>
+                        <option value="yes_everywhere" <?php selected( 'yes_everywhere', get_user_meta( $user->ID, 'use_opendyslexic', true ) ); ?>><?php _e('Use both on the website and in the admin', 'opendyslexic');?></option>
+                        <option value="yes_websiteonly" <?php selected( 'yes_websiteonly', get_user_meta( $user->ID, 'use_opendyslexic', true ) ); ?>><?php _e('Only use on the website', 'opendyslexic');?></option>
+                        <option value="yes_adminonly" <?php selected( 'yes_adminonly', get_user_meta( $user->ID, 'use_opendyslexic', true ) ); ?>><?php _e('Only use within the admin', 'opendyslexic');?></option>
+                    </select>
+	</td>
+        </tr>
+        </table>
+    <?php
+}
+
+
+function update_profile_use_opendyslexic($user_id) {
+     if ( current_user_can('edit_user',$user_id) )
+         update_usermeta($user_id, 'use_opendyslexic', $_POST['use_opendyslexic']);
+}
+
+function add_opendyslexic_css()
+{
+$user_ID = get_current_user_id(); 
+$use_opendyslexic = get_user_meta($user_ID, 'use_opendyslexic', true );
+if ($use_opendyslexic=="yes_everywhere" || $use_opendyslexic=="yes_websiteonly"){
+?>
+<style type="text/css">
+@font-face { font-family: open-dyslexic; src: url('<?= plugin_dir_url( __FILE__ );?>OpenDyslexic-Regular.ttf'); }
+* { font-family: open-dyslexic !important }
+</style>
+<?php
+}
+}
+
+function add_opendyslexic_css_admin()
+{
+$user_ID = get_current_user_id(); 
+$use_opendyslexic = get_user_meta($user_ID, 'use_opendyslexic', true);
+if ($use_opendyslexic=="yes_everywhere" || $use_opendyslexic=="yes_adminonly"){
+?>
+<style type="text/css">
+@font-face { font-family: open-dyslexic; src: url('<?= plugin_dir_url( __FILE__ );?>OpenDyslexic-Regular.ttf'); }
+* { font-family: open-dyslexic !important }
+</style>
+<?php
+}
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Name ===
+Contributors: 120w
+Donate link: https://frankleonard.nl/wp-opendyslexic/
+Tags: dyslexia, font, admin
+Requires at least: 3.0.1
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows registered users to enable the OpenDyslexic font in their profile.
+
+== Description ==
+
+This plugin allows registered users to enable the OpenDyslexic font (see: http://www.opendyslexic.org/) on your Wordpress site.
+This free font is designed to help people with dyslexia read more easily.
+After activation this plugin adds an extra option to the profile page, where users can choose to use the OpenDyslexic font on
+the website and/or within the admin, or not at all.
+
+== Installation ==
+
+0. Download and unzip
+1. Upload the folder from the plugin to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Do you support other languages? =
+
+So far this plugins is only available English and Dutch are included. You are free to create a translation for your language.
+The required .POT file is in the 'languages' folder.
+
+= Will the OpenDyslexic font be visible to all visitors? =
+
+The font is only visible for the registered users who have enabled the font on their user profile. All other users and visitors will see the regular font as defined by your theme/CSS.
+
+== Known bugs ==
+
+* Some of the icons from the admin menu don't show because those symbols are not (yet) included in the OpenDyslexic font.
+
+== Screenshots ==
+None.
+
+== Upgrade Notice ==
+None.
+
+== Changelog ==
+
+= 1.0 =
+* Initial version
